1f295db4d127c0874cbda8665ca091988649e3f3
Boyko Alexey <elorian@yandex.ru>
2021-11-01 12:56:45 +0300

    correct names of variables

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -6,10 +6,10 @@ def passcard_info_view(request, passcode):
     """
     Функция отображения информации пропуска сотрудника
     """
-    passcard = Visit.objects.filter(passcard__passcode=passcode)
-    name = passcard[0].passcard.owner_name
+    list_of_visits = Visit.objects.filter(passcard__passcode=passcode)
+    name_of_employee = list_of_visits[0].passcard.owner_name
     context = {
-        'passcard': passcard,
-        'name': name,
+        'list_of_visits': list_of_visits,
+        'name': name_of_employee,
     }
     return render(request, 'passcard_info.html', context)
