85386e41069626405f9379463817bb467867d425
Boyko Alexey <elorian@yandex.ru>
2021-11-10 18:00:21 +0300

    correct name of function(and variables in it) in active_passcards_view.py

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -2,12 +2,12 @@ from datacenter.models import Passcard
 from django.shortcuts import render
 
 
-def active_passcards_view(request):
+def take_active_passcards(request):
     """
     Функция отображения сотрудников с актуальным пропуском
     """
-    all_passcards = Passcard.objects.filter(is_active=True)
+    all_active_passcards = Passcard.objects.filter(is_active=True)
     context = {
-        'active_passcards': all_passcards,
+        'active_passcards': all_active_passcards,
     }
     return render(request, 'active_passcards.html', context)
